e44f4901acc133bd0340ded925b18eccceb2979e
tedtower <34298083+tedtower@users.noreply.github.com>
2017-12-12 10:06:27 +0800

    Add files via upload

---
+++ EternalImmortality.java
@@ -0,0 +1,15 @@
+//Problem 869B - The Eternal Immortality
+import java.util.Scanner;
+
+public class EternalImmortality
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		long a = in.nextLong(), b = in.nextLong();
+		long ans = 1;
+		for (long i = a + 1; i <= b && ans != 0; i++)
+			ans = ans * i % 10;
+		System.out.println(ans);
+	}
+}
---
+++ FairG.java
@@ -0,0 +1,20 @@
+//Problem 864A - Fair Game
+import java.util.*;
+public class FairG {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+	int n=scan.nextInt();
+	int c=0;
+	int[] A=new int[101],C=new int[n];
+	for(int i=0;i<n;i++){
+	   int a=scan.nextInt();
+	   if(A[a]==0) C[c++]=a;
+	   A[a]++;
+	}
+	if(c==2&&C[0]!=C[1]&&A[C[0]]==n/2&&A[C[1]]==n/2){
+	   System.out.println("YES");
+	   System.out.println(C[0]+" "+C[1]);
+	}
+	else System.out.println("NO");
+    }
+}
